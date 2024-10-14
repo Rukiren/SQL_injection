92f9b473ed9be9819158c77c4a81a6b9b5b1becb
Ruki <75606879+Rukiren@users.noreply.github.com>
2024-10-14 20:31:08 +0800

    Update login_index.php

--- login_index.php
+++ login_index.php
@@ -21,18 +21,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     $user = $_POST['username'];
     $pass = $_POST['password'];
     
-    // 這裡存在 SQL Injection 漏洞
     $sqlQuery = "SELECT * FROM users WHERE username='$user' AND password='$pass'";
-    $result = $conn->query($sqlQuery);
-
-    if ($result) {
+    
+    try {
+        $result = $conn->query($sqlQuery);
+        if ($result === false) {
+            throw new Exception($conn->error);
+        }
         if ($result->num_rows > 0) {
             $loginMessage = "Login successful!";
         } else {
             $loginMessage = "Invalid username or password.";
         }
-    } else {
-        $errorMessage = "Error: " . $conn->error;
+    } catch (Exception $e) {
+        $errorMessage = "Error: " . $e->getMessage();
     }
 }
 
@@ -40,18 +42,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register'])) {
     $newUser = $_POST['new_username'];
     $newPass = $_POST['new_password'];
 
-    // 插入新用戶
     $sqlQuery = "INSERT INTO users (username, password) VALUES ('$newUser', '$newPass')";
-    if ($conn->query($sqlQuery) === TRUE) {
-        $registerMessage = "Registration successful!";
-    } else {
-        $registerMessage = "Error: " . $conn->error;
+    
+    try {
+        if ($conn->query($sqlQuery) === TRUE) {
+            $registerMessage = "Registration successful!";
+        } else {
+            throw new Exception($conn->error);
+        }
+    } catch (Exception $e) {
+        $errorMessage = "Error: " . $e->getMessage();
     }
 }
 
 $conn->close();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -64,19 +69,18 @@ $conn->close();
         Password: <input type="password" name="password"><br>
         <input type="submit" name="login" value="Login">
     </form>
-    <p><?php echo $loginMessage; ?></p>
-
+    <p><?php echo htmlspecialchars($loginMessage); ?></p>
+    
     <h2>Register</h2>
     <form method="post" action="">
         Username: <input type="text" name="new_username"><br>
         Password: <input type="password" name="new_password"><br>
         <input type="submit" name="register" value="Register">
     </form>
-    <p><?php echo $registerMessage; ?></p>
-
+    <p><?php echo htmlspecialchars($registerMessage); ?></p>
+    
     <h2>Operation Details</h2>
-    <p>Last SQL Query: <?php echo $sqlQuery; ?></p>
-    <p>Error Message: <?php echo $errorMessage; ?></p>
+    <p>Last SQL Query: <?php echo htmlspecialchars($sqlQuery); ?></p>
+    <p>Error Message: <?php echo htmlspecialchars($errorMessage); ?></p>
 </body>
 </html>
-
